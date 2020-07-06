aabb33264b59ac9a3e4fb0376b573188f5fafd36
DiksonRodrigues <55064590+DiksonRodrigues@users.noreply.github.com>
2020-07-06 20:27:29 -0300

    mudando

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,9 @@ const Index = () => {
   return(
   <div>
     <p className='mt-12 text-center'  >Estamos sempre atentos para ouvir nossos clientes e melhorarmos nossos servicos. E ainda te dar Prêmios</p>
-  <p>Teste</p>
     <div className='text-center my-12' >
       <Link href='/pesquisa'>
-       <a className='bg-blue-300 px-6 py-4 font-bold rounded-lg shadow-lg ' >Dar opiniäo ou sugestäo</a>
+       <a className='bg-blue-300 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow' >Faça sua Crítica ou Sugestão </a>
        </Link>
     </div>
     <p className='mt-12 text-center'  >Mensagem de desconto</p>
--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -3,12 +3,12 @@ import Link from 'next/link'
 
 const Pesquisa = () => {
     return (
-        <div>
-            <h1>Pesquisa</h1>
-            <div>
-                <Link href='/'>
-                   <a>Pagina Inicial</a> 
-                </Link>
+        <div className='pt-6' >
+            <h1 className='text-center font-bold my-6 text-2xl mb-6' >Críticas e Sugestões</h1>
+            <p className='text-center'>Esse estabelecimento está atento a todas as sugestões dos nossos clientes.<br/> E estamos prontos para melhor atender-los </p>
+            <div className='w-1/5 mx-auto' >
+              <label className='font-bold' >Seu Nome:</label>
+              <input type='text' className='p-4 block shadow bg-blue-100 my-2 rounded' />
             </div>
         </div>
     )
