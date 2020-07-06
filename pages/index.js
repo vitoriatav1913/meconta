d7413556a9fd2180d8d5b6a259126a7da2009acb
DiksonRodrigues <55064590+DiksonRodrigues@users.noreply.github.com>
2020-07-06 20:30:14 -0300

    Alterado teste

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ const Index = () => {
     <p className='mt-12 text-center'  >Estamos sempre atentos para ouvir nossos clientes e melhorarmos nossos servicos. E ainda te dar Prêmios</p>
     <div className='text-center my-12' >
       <Link href='/pesquisa'>
-       <a className='bg-blue-300 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow' >Faça sua Crítica ou Sugestão </a>
-       </Link>
+       <a className='bg-blue-300 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow' >Faça sua Crítica ou   Sugestão </a>
+        </Link>
     </div>
     <p className='mt-12 text-center'  >Mensagem de desconto</p>
  </div>)
